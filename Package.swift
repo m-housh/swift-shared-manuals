8c34354c72e9155a40bf46c40a726a088b1adaf9
Michael Housh <michael@mhoush.com>
2026-03-05 14:17:43 -0500

    feat: Adds platforms to package

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
   name: "swift-shared-manuals",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
+    .tvOS(.v13),
+    .watchOS(.v6),
+  ],
   products: [
     .library(name: "AuthClient", targets: ["AuthClient"]),
     .library(name: "SharedDatabase", targets: ["SharedDatabase"]),
